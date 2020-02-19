facaf1477772ffeffe67a58dbb79994f7c19eac3
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-02-19 15:35:40 +0600

    Design home activity

--- app/src/main/java/io/horizontalsystems/lightningwallet/modules/home/HomeActivity.kt
+++ app/src/main/java/io/horizontalsystems/lightningwallet/modules/home/HomeActivity.kt
@@ -3,11 +3,26 @@ package io.horizontalsystems.lightningwallet.modules.home
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.horizontalsystems.lightningwallet.R
+import io.horizontalsystems.views.TopMenuItem
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setActionBar()
+    }
+
+    private fun setActionBar() {
+        val leftBtn = TopMenuItem(R.drawable.ic_share, onClick = {
+            onBackPressed()
+        })
+
+        val rightBtn = TopMenuItem(R.drawable.settings, onClick = {
+            
+        })
+
+        shadowlessToolbar.bind(null, leftBtnItem = leftBtn, rightBtnItem = rightBtn)
     }
 }
